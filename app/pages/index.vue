d6c650cb2be48a2c9ed2359c1cb9180ad6ee3473
Paulo Rafael <paulorafaelbatista777@gmail>
2025-07-29 14:48:37 -0300

    Feat: change button into icons, changes in the visual, execution correction removeTask fuction

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+
 const input = ref('')
 const tasks = ref<string[]>([])
+
 function addTask() {
     if (input.value.trim() !== '') {
         tasks.value.push(input.value.trim())
         input.value = ''
     }
 }
-
 function removeTask(index: number){
     tasks.value.splice(index, 1)
 }
@@ -17,20 +18,15 @@ function removeTask(index: number){
 
 <template>
     <div class="flex flex-col items-center mt-8">
-        <BaseCard rounded="lg" class="flex justify-between gap-2 p-2">
-            <BaseInput v-model="input" type="text" placeholder="Crie uma tarefa aqui..." rounded="full" class="">
-            </BaseInput>
-            <BaseButton @click="addTask" rounded="full" class="">
-                <Icon name="solar:add-circle-line-duotone" width="24" height="24" /> 
-            </BaseButton>
+        <BaseCard rounded="lg" class="flex justify-between items-center gap-2 p-2 w-full max-w-xl">
+            <BaseInput v-model="input" type="text" placeholder="Crie uma tarefa aqui..." rounded="full" class=""/>
+            <Icon name="solar:add-circle-line-duotone" class="size-10 cursor-pointer" @click="addTask"/>
         </BaseCard>
         <BaseCard rounded="lg" class="w-full max-w-xl h-64 mt-2">
-            <BaseList class="">
-                <li v-for="(task, index) in tasks">
-                    {{ index + 1 }}. {{ task }}
-                    <BaseButton @click="removeTask" rounded="full">
-                        <Icon name="solar:close-square-bold" width="24" height="24" />
-                    </BaseButton>
+            <BaseList class="p-2">
+                <li v-for="(task, index) in tasks" class="flex items-center justify-between wrap-break-word">
+                    <p>{{ index + 1 }}. {{ task }}</p> 
+                    <Icon name="solar:close-square-bold" class="size-6 cursor-pointer" @click="removeTask(index)" />
                 </li>
             </BaseList>
         </BaseCard>

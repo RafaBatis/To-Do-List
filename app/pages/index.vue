8be7716dd4969a8f57dba256aacf88e9f8cd6a00
Paulo Rafael <paulorafaelbatista777@gmail>
2025-08-07 17:30:24 -0300

    feat: creating tasks using 'enter', focus and styling fix

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, defineExpose } from 'vue'
 
 const input = ref('')
+const inputRef = ref<HTMLInputElement | null>(null)
 const tasks = ref<string[]>([])
 const completed = ref<string[]>([])
+
+defineExpose({
+  focus: () => inputRef.value?.focus()
+})
 
 function addTask() {
     if (input.value.trim() !== '') {
         tasks.value.push(input.value.trim())
         input.value = ''
+        inputRef.value?.focus()
     }
 }
 function removeTask(index: number){
@@ -27,13 +33,19 @@ function completedTask(index: number){
         completed.value.splice(a, 1)
     }
 }
+function enterTask(e: KeyboardEvent){
+    if (e.key === 'Enter') {
+        addTask()
+    }
+    
+}
 
 </script>
 
 <template>
     <div class="flex flex-col items-center mt-8">
         <BaseCard rounded="lg" class="flex justify-between items-center gap-2 p-2 w-full max-w-xl">
-            <BaseInput v-model="input" type="text" placeholder="Crie uma tarefa aqui..." rounded="full" class=""/>
+            <BaseInput v-model="input" type="text" placeholder="Crie uma tarefa aqui..." rounded="full" class="" ref="inputRef" @keydown="enterTask"/>
             <Icon name="solar:add-circle-line-duotone" class="size-10 cursor-pointer" @click="addTask"/>
         </BaseCard>
         <BaseCard rounded="lg" class="nui-slimscroll w-full max-w-xl overflow-y-auto h-96 mt-2">
@@ -42,12 +54,12 @@ function completedTask(index: number){
                 <p class="flex items-center gap-2">Tarefas concluídas <span class="flex items-center justify-center bg-zinc-500 rounded-full text-sm px-2 py-0.5">{{ completed.length }}</span> de <span class="flex items-center justify-center bg-zinc-500 rounded-full text-sm px-2 py-0.5">{{ tasks.length }}</span> </p>
             </div>
             <BaseList class="p-2">
-                <li v-for="(task, index) in tasks" class="flex items-center justify-between text-wrap">
+                <li v-for="(task, index) in tasks" class="flex items-start justify-between text-wrap">
                     <div class="flex items-center gap-2">
                     <Icon :name="completed.includes(task) ? 'solar:check-circle-bold' : 'solar:check-circle-line-duotone'" class="size-6 cursor-pointer" @click="completedTask(index)"> </Icon>
-                    <p :class="{'line-through': completed.includes(task)}">{{ index + 1 }}. {{ task }}</p>
+                    <p :class="{'line-through': completed.includes(task)}" class="break-words max-w-[80%]">{{ index + 1 }}. {{ task }}</p>
                     </div>
-                    <Icon name="solar:close-square-bold" class="size-6 cursor-pointer" @click="removeTask(index)"/>
+                    <Icon name="solar:close-square-bold" class="size-6 cursor-pointer flex-shrink-0" @click="removeTask(index)"/>
                 </li>
             </BaseList>
         </BaseCard>
